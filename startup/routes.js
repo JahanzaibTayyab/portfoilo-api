77e422c964a52724be8524ac47a13240c86b607c
Jahanzaib <jahanzaib.tayyab@devigital.com>
2021-03-31 16:21:12 +0500

    unit testing for token generation

--- startup/routes.js
+++ startup/routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+var morgan = require("morgan");
 
 const indexRouter = require("../routes/index");
 const usersRouter = require("../routes/users");
@@ -19,7 +19,7 @@ module.exports = function (app) {
   app.use(express.json());
   app.set("views", path.join(__dirname, "views"));
   app.set("view engine", "jade");
-  app.use(logger("dev"));
+  app.use(morgan("dev"));
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
